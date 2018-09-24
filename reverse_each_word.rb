f482904dccb36fa8a37831aea983ab28b955d149
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-24 23:52:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,10 @@ def reverse_each_word(x)
   ar.each { |z| arr << z.reverse}
   return arr.join(" ")
 end
+
+def reverse_each_word(x)
+  arr = []
+  ar=x.split(" ")
+  ar.each { |z| arr << z.reverse}
+  return arr.join(" ")
+end
